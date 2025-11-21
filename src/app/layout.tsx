70a6f2e1bff2d0375192662383534a1fb0b90927
ashutosh-gitdev <ashutoshzepeto@gmail.com>
2025-11-21 19:22:13 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+//production URL
+const appUrl = "https://search-by-fid.vercel.app";
+
+//Mini App Embed JSON using public images
+const embed = {
+  version: "1",
+  imageUrl: `${appUrl}/hero.png`, // Uses hero image for the share card (3:2 ratio recommended)
+  button: {
+    title: "Start Searching",
+    action: {
+      type: "launch_frame",
+      name: "Search by fid",
+      url: appUrl,
+      splashImageUrl: `${appUrl}/splash.png`, 
+      splashBackgroundColor: "#F5F8FF",
+    },
+  },
+};
+
 export const metadata: Metadata = {
   title: "Search by FID",
   description: "Search user by their FID",
+  other: {
+    "fc:frame": JSON.stringify(embed),
+  },
 };
 
 export default function RootLayout({
